b34dfca7df0f8843f7666d27f2e9646ec138c9d4
Matt <matta628@gmail.com>
2026-04-13 16:57:59 -0400

    Add build version log

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+console.log('🎸 Tastemaker build: 2026-04-13-v2')
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
